23963d4312f2805de6619f3385a2c717db8f6f17
Leo05-gif <leonardo.kovtun@itiszuccante.edu.it>
2025-10-18 15:31:48 +0200

    Rimosso il caso della "combinazione errata" in quanto risultava ridondante con l'aggiunta del feedback al di sotto dei buttoni.

--- inferiormind/lib/main.dart
+++ inferiormind/lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -7,45 +9,21 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Inferior Mind',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.red),
+        useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Inferior Mind'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -54,69 +32,186 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  final List<Color> _colours = [Colors.red, Colors.blue, Colors.yellow, Colors.green];
+  static int _maxAttempts = 3;
 
-  void _incrementCounter() {
+  // BUTTONS RELATED
+  List<int> _counters = List.filled(4, 0);
+  List<Color> _currentColour = List.filled(4, Colors.grey);
+  List<Color> _sequence = [];
+
+  // TEXT
+  List<String> _guessesText = List.filled(4, "");
+  String _statusText = "";
+  final String _attemptsText = "Tentativi Rimasti: ";
+
+  // FLAGS
+  bool _gameOverFlag = false;
+  bool _vittoryFlag = false;
+
+  int _attempts = _maxAttempts;
+
+  @override
+  void initState() {
+    super.initState();
+    _sequence = _generateSequence();
+  }
+
+  List<Color>  _generateSequence() {
+    List<Color> newSequence = [];
+    for (var i = 0; i < _colours.length; i++) {
+      newSequence.add(_colours[Random().nextInt(_colours.length)]);
+    }
+    return newSequence;
+  }
+
+  void _updateButton(int index) {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      if (_counters[index] > _colours.length - 1) {
+        _counters[index] = 0;
+      }
+      _currentColour[index] = _colours[_counters[index]];
+      _counters[index]++;
+    });
+  }
+
+  void _checkGameOver() {
+    _attempts--;
+
+    for (int i = 0; i < _currentColour.length; i++) {
+      if (_currentColour[i].toString() == _sequence[i].toString()) {
+        _guessesText[i] = "INDOVINATO";
+      } else {
+        _guessesText[i] = "SBAGLIATO";
+      }
+    }
+    if (_currentColour.toString() == _sequence.toString()) {
+      _vittoryFlag = true;
+    }
+    if (_attempts == 0) {
+      _gameOverFlag = true;
+    }
+  }
+
+  void _updateStatusText() {
+    setState(() {
+      if (_vittoryFlag) {
+        _statusText = "Hai Vinto!";
+      }
+      else if (_gameOverFlag) {
+        _statusText = "Hai Perso!";
+      } 
+    });
+  }
+
+  void _checkGame() {
+    _checkGameOver();
+    _updateStatusText();
+    if (_vittoryFlag || _gameOverFlag) {
+      Future.delayed(Duration(seconds: 2), () {
+        _resetGame();
+      });
+    }
+  }
+
+  void _resetGame() {
+    setState(() {
+      // RESET BUTTONS
+      _counters = List.filled(4, 0);
+      _currentColour = List.filled(4, Colors.grey);
+
+      // RESET TEXT
+      _guessesText = List.filled(4, "");
+      _statusText = "";
+
+      // RESET FLAGS
+      _vittoryFlag = false;
+      _gameOverFlag = false;
+
+      _attempts = _maxAttempts;
+      _sequence = _generateSequence();
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
+        shadowColor: Theme.of(context).shadowColor,
+        elevation: 4,
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+      body: Stack(
+        children: [
+          Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    FloatingActionButton(
+                      onPressed: () {
+                        _updateButton(0);
+                      },
+                      backgroundColor: _currentColour[0],
+                    ),
+                    FloatingActionButton(
+                      onPressed: () {
+                        _updateButton(1);
+                      },
+                      backgroundColor: _currentColour[1],
+                    ),
+                    FloatingActionButton(
+                      onPressed: () {
+                        _updateButton(2);
+                      },
+                      backgroundColor: _currentColour[2],
+                    ),
+                    FloatingActionButton(
+                      onPressed: () {
+                        _updateButton(3);
+                      },
+                      backgroundColor: _currentColour[3],
+                    ),
+                  ],
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Text(_guessesText[0]),
+                    Text(_guessesText[1]),
+                    Text(_guessesText[2]),
+                    Text(_guessesText[3]),
+                  ],
+                )
+              ],
             ),
-          ],
-        ),
+          ),
+          Align(
+            alignment: Alignment.topCenter,
+            child: Text(
+              _statusText,
+              style: (TextStyle(fontSize:24)),
+            ),
+          ),
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: Text(
+              _attemptsText + _attempts.toString(),
+              style: (TextStyle(fontSize:24)),
+            ),
+          )
+        ],
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+        onPressed: () {
+          if (!_vittoryFlag && !_gameOverFlag) {
+            _checkGame();
+          }
+        },
+        child: Icon(Icons.add_task),
+      ),
     );
   }
 }
