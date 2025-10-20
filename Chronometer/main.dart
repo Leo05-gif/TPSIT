1747cab9e1ee2de52c48b10c997402d6bc9a6974
Leo <leonardo.kovtun@itiszuccante.edu.it>
2025-10-20 11:41:51 +0200

    Aggiunta la classe del tempo

--- Chronometer/main.dart
+++ Chronometer/main.dart
@@ -1,10 +1,27 @@
 import 'dart:async';
 
+class Time {
+  int seconds = 0;
+  int minutes = 0;
+  int hours = 0;
+}
+
 class Timer {
+  Time time = Time();
+
   Stream<int> secondGenerator(Stream<String> tick) async* {
-    int seconds = 0;
     await for (final s in tick) {
-      yield seconds++;
+      if (time.seconds == 60) {
+        time.minutes++;
+        time.seconds = 0;
+      }
+
+      if (time.minutes == 60) {
+        time.hours++;
+        time.minutes = 0;
+      }
+
+      yield time.seconds++;
     }
   }
 
