726093228d1e748b6054469f52a180d604a91205
Ludovico Bellio <ludovico.bellio@itiszuccante.edu.it>
2025-10-20 11:15:18 +0200

    Spostate le funzioni riguardanti il timer in una classe dedicata

--- Chronometer/main.dart
+++ Chronometer/main.dart
@@ -1,23 +1,26 @@
 import 'dart:async';
 
+class Timer {
+  Stream<int> secondGenerator(Stream<String> tick) async* {
+    int seconds = 0;
+    await for (final s in tick) {
+      yield seconds++;
+    }
+  }
+
+  Stream<String> tickGenerator(Duration inverval) async* {
+    while (true) {
+      await Future.delayed(inverval);
+      yield "tick";
+    }
+  }
+}
+
 main() async {
-  var t = tickGenerator(const Duration(seconds: 1));
-
-  timer(t).listen((data) {
-    print(data);
-  });
-}
-
-Stream<int> timer(Stream<String> tick) async* {
-  int seconds = 0;
-  await for (final s in tick) {
-    yield seconds++;
-  }
-}
-
-Stream<String> tickGenerator(Duration inverval) async* {
-  while (true) {
-    await Future.delayed(inverval);
-    yield "tick";
-  }
+  Timer timer = Timer();
+  timer.secondGenerator(timer.tickGenerator(const Duration(seconds: 1))).listen(
+    (data) {
+      print(data);
+    },
+  );
 }
