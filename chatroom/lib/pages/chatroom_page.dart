143e8b358eb29b8e0755400970a4127f2be3ae0e
Leo05-gif <leonardo.kovtun@itiszuccante.edu.it>
2025-11-21 16:17:56 +0100

    implementazione della connessione al server e visualizzazione dei messaggi

--- chatroom/lib/pages/chatroom_page.dart
+++ chatroom/lib/pages/chatroom_page.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:chatroom/data/notifiers.dart';
 import 'package:flutter/material.dart';
 
@@ -10,6 +12,47 @@ class _ChatroomPageState extends State<ChatroomPage> {
   TextEditingController controller = TextEditingController();
 
   final List<String> messages = [];
+  late Socket socket;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+
+    Socket.connect("localhost", 3000).then(
+      (Socket sock) {
+        socket = sock;
+        sock.write("username:${usernameNotifier.value}");
+        socket.listen(
+          dataHandler,
+          onError: errorHandler,
+          onDone: doneHandler,
+          cancelOnError: false,
+        );
+      },
+      onError: (e) {
+        print("Unable to connect: $e");
+        exit(1);
+      },
+    );
+    stdin.listen(
+      (data) => socket.write(String.fromCharCodes(data).trim() + '\n'),
+    );
+  }
+
+  void dataHandler(data) {
+    setState(() {});
+    messages.add(String.fromCharCodes(data).trim());
+  }
+
+  void errorHandler(error, StackTrace trace) {
+    print(error);
+  }
+
+  void doneHandler() {
+    socket.destroy();
+    exit(0);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -31,8 +74,11 @@ class _ChatroomPageState extends State<ChatroomPage> {
             TextField(
               controller: controller,
               onEditingComplete: () {
+                socket.write(controller.text + '\n');
                 setState(() {
-                  messages.add(controller.text);
+                  messages.add(
+                    '${usernameNotifier.value}: ${controller.text} ',
+                  );
                   controller.clear();
                 });
               },
