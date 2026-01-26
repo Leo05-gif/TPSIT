5ed6b18eddcc55ffc94d1c78b8609a0366207a34
Leo05-gif <leonardo.kovtun@itiszuccante.edu.it>
2026-01-26 10:21:49 +0100

    creazione progetto

---
+++ todosql/lib/main.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import 'model.dart';
+import 'notifier.dart';
+import 'widgets.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Todo List',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(colorScheme: .fromSeed(
+        seedColor: Colors.teal,
+        brightness: Brightness.dark,
+        ),
+      ),
+      home: ChangeNotifierProvider<TodoListNotifier>(
+        create: (notifier) => TodoListNotifier(),
+        child: const MyHomePage(title: 'Todo List'),
+      ),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    final TodoListNotifier notifier = context.watch<TodoListNotifier>();
+
+    return Scaffold(
+      appBar: AppBar(
+        shadowColor: Theme.of(context).shadowColor,
+        elevation: 4,
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: ListView.builder(
+          padding: const EdgeInsets.symmetric(vertical: 8.0),
+          itemCount: notifier.length,
+          itemBuilder: (context, index) {
+            Todo todo = notifier.getTodo(index);
+            return TodoItem(todo: todo);
+          },
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => notifier.addTodo(),
+        tooltip: 'add todo',
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
