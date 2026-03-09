9179ec15edbc8e7e34bdde0c63859ab2a77f9a89
Leo05-gif <leonardo.kovtun@itiszuccante.edu.it>
2026-03-09 11:44:48 +0100

    parte 2

--- test-warehouse/lib/test_warehouse.dart
+++ test-warehouse/lib/test_warehouse.dart
@@ -2,10 +2,11 @@ import 'dart:convert';
 
 import 'package:http/http.dart' as http;
 
+import 'Product.dart';
+
 Future<void> main() async {
   var url = Uri.parse('http://127.0.0.1:3000/products');
   var response = await http.get(url);
-  print(response.body);
   print(response.statusCode);
   var responsebella = await http.post(
     url,
@@ -18,6 +19,15 @@ Future<void> main() async {
       "n": 1
     }),
   );
-  print(responsebella.body);
-  print(responsebella.statusCode);
+
+  var data = jsonDecode(response.body);
+  List<Product> lista = [];
+
+  for (var product in data) {
+    lista.add(Product.fromMap(product));
+  }
+
+  print(lista[0].price);
+
+
 }
